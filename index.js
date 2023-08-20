c2f3369d6e5a1593d2d598a79e07f123d1092c28
Pedram <p.mollarafie1@gmail.com>
2023-08-20 16:54:31 +0330

    added base photos and fontawesome icons

---
+++ index.js
@@ -0,0 +1,202 @@
+const homeMovie = [
+    {
+        id: 'movie-uuid-0',
+        movie_name: 'Tamasha',
+        year: 2015,
+        score: 7.3,
+        favorite: false,
+    },
+]
+
+
+const mustWatch = [
+    {
+        id: 'movie-uuid-1',
+        movie_name: 'Kumbalangi Nights',
+        year: 2019,
+        score: 8.6,
+        favorite: false,
+    },
+    {
+        id: 'movie-uuid-2',
+        movie_name: 'The Lunchbox',
+        year: 2013,
+        score: 7.7,
+        favorite: true,
+    },
+    {
+        id: 'movie-uuid-3',
+        movie_name: 'Haider',
+        year: 2014,
+        score: 8.4,
+        favorite: true,
+    },
+    {
+        id: 'movie-uuid-4',
+        movie_name: 'Aamis',
+        year: 2019,
+        score: 8.0,
+        favorite: true,
+    },
+    {
+        id: 'movie-uuid-5',
+        movie_name: 'Court',
+        year: 2014,
+        score: 7.6,
+        favorite: true,
+    },
+    {
+        id: 'movie-uuid-6',
+        movie_name: 'Masaan',
+        year: 2015,
+        score: 8.1,
+        favorite: true
+    },
+    {
+        id: 'movie-uuid-7',
+        movie_name: 'Kai Po Che',
+        year: 2013,
+        score: 7.9,
+        favorite: false
+    },
+]
+
+
+const recommended = [
+    {
+        id: 'movie-uuid-8',
+        movie_name: '96',
+        year: 2018,
+        score: 8.6,
+        favorite: false
+    },
+    {
+        id: 'movie-uuid-9',
+        movie_name: 'Swades',
+        year: 2004,
+        score: 8.2,
+        favorite: true
+    },
+    {
+        id: 'movie-uuid-10',
+        movie_name: 'Andhadhun',
+        year: 2018,
+        score: 8.2,
+        favorite: true
+    },
+    {
+        id: 'movie-uuid-11',
+        movie_name: 'Sarpatta Paramba...',
+        year: 2021,
+        score: 8.8,
+        favorite: false
+    },
+    {
+        id: 'movie-uuid-12',
+        movie_name: 'Piku',
+        year: 2015,
+        score: 7.6,
+        favorite: true
+    },
+    {
+        id: 'movie-uuid-13',
+        movie_name: 'Hotel Mumbai',
+        year: 2018,
+        score: 7.6,
+        favorite: false
+    },
+    {
+        id: 'movie-uuid-7',
+        movie_name: 'Kai Po Che',
+        year: 2013,
+        score: 7.9,
+        favorite: false
+    },
+]
+
+
+const classics = [
+    {
+        id: 'movie-uuid-14',
+        movie_name: 'Dil Chahta Hai',
+        year: 2001,
+        score: 8.1,
+        favorite: false
+    },
+    {
+        id: 'movie-uuid-15',
+        movie_name: '3 Idiots',
+        year: 2009,
+        score: 8.5,
+        favorite: true
+    },
+    {
+        id: 'movie-uuid-16',
+        movie_name: 'Rockstar',
+        year: 2011,
+        score: 7.8,
+        favorite: true
+    },
+    {
+        id: 'movie-uuid-17',
+        movie_name: 'Jaane Tu... Ya Jaa...',
+        year: 2008,
+        score: 7.4,
+        favorite: true
+    },
+    {
+        id: 'movie-uuid-18',
+        movie_name: 'Dilwale Dulhaniya...',
+        year: 1995,
+        score: 7.2,
+        favorite: true
+    },
+    {
+        id: 'movie-uuid-19',
+        movie_name: 'My Name is Khan',
+        year: 2010,
+        score: 8.0,
+        favorite: true
+    },
+    {
+        id: 'movie-uuid-7',
+        movie_name: 'Kai Po Che',
+        year: 2013,
+        score: 7.9,
+        favorite: false
+    },
+]
+
+
+const similar = [
+    {
+        id: 'movie-uuid-6',
+        movie_name: 'Masaan',
+        year: 2015,
+        score: 8.1,
+        favorite: true
+    },
+    {
+        id: 'movie-uuid-20',
+        movie_name: 'Dil Dhadakne do',
+        year: 2004,
+        score: 8.2,
+        favorite: true
+    },
+    {
+        id: 'movie-uuid-21',
+        movie_name: 'Aligarh',
+        year: 2018,
+        score: 8.2,
+        favorite: true
+    },
+    {
+        id: 'movie-uuid-22',
+        movie_name: 'Karwaan',
+        year: 2021,
+        score: 8.8,
+        favorite: false
+    },
+]
+
+
